fbe157a584b43e017f1a84a3c9cdfc06f7c7b959
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-06 19:37:53 +0300

    Рекомендации по весу

---
+++ Рекомендации по весу/Рекомендации по весу/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Рекомендации_по_весу
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой рост");
+            int growth = int.Parse(Console.ReadLine());
+            int hundred = 100;
+            double sum = growth - hundred;
+            Console.WriteLine("Напишите свой вес");
+            int weight = int.Parse(Console.ReadLine());
+            if (sum > weight)
+            {
+                Console.WriteLine("Вам стоит набрать вес");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Вам стоит похудеть");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
