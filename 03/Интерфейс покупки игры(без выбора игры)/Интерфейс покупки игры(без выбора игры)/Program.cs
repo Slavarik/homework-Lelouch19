675f2e6621b80d39df8e9fc91b4277efc349d4e3
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-07 12:06:39 +0300

    Интерфейс покупки игры(без выбора игры)

---
+++ 03/Интерфейс покупки игры(без выбора игры)/Интерфейс покупки игры(без выбора игры)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Интерфейс_покупки_игры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свой баланс");
+            int balance = int.Parse(Console.ReadLine());
+            if (balance >= 2000)
+            {
+                Console.WriteLine("Поздравляем с покупкой!");
+                Console.ReadLine();
+                int balance1 = balance - 2000;
+                Console.WriteLine("Теперь ваш баланс составляет " + balance1 + " рублей");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("На вашем балансе недостаточно средств");
+                Console.ReadLine();
+            }
+        }
+    }
+}
