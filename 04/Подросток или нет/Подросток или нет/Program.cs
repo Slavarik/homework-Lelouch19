5f717e4217c2dfd0e097ef25a2e8855d3efbe5f7
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-10 21:32:44 +0300

    Подросток ты или нет?

---
+++ 04/Подросток или нет/Подросток или нет/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Подросток_или_нет
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой возраст");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 10)
+            {
+                if (age < 20)
+                {
+                    Console.WriteLine("Поздравляю,ты подросток!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ваши подростковые годы уже прошли :( ");
+                    Console.ReadLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Увы,но ты ещё не подросток");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
