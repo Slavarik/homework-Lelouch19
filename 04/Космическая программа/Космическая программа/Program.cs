0fd227e09d214b44f16dcf4ad769ae926b999657
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-10 16:13:11 +0300

    Космическая программа

---
+++ 04/Космическая программа/Космическая программа/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Космическая_программа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите сколько процентов кислорода в составе воздуха");
+            int Oxygen = int.Parse(Console.ReadLine());
+            if (Oxygen > 20)
+            {
+                Console.WriteLine("Напишите среднюю температуру поверхность планеты");
+                int t = int.Parse(Console.ReadLine());
+                if (t > 15)
+                {
+                    Console.WriteLine("Поздравляю!Вы нашли пригодную для жизни планету!Скорее летите туда!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("На планете слишком холодно.Придётся остаться на Земле ");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("На планете слишком мало кислорода.Лучше останьтесь дома ;(");
+                Console.ReadLine();
+            }
+        }
+    }
+}
