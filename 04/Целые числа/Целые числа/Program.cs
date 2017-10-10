cd3acbec6aa21ed9dfe0fd663c69183755b972b1
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-10 21:54:19 +0300

    Наибольшее целое число

---
+++ 04/Целые числа/Целые числа/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Целые_числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье целое число");
+            int c = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine("Наибольшее целое число - " + a);
+                    Console.ReadLine();
+                }
+            }
+            if (b > c)
+            {
+                if (b > a)
+                {
+                    Console.WriteLine("Наибольшее целое число - " + b);
+                    Console.ReadLine();
+                }
+            }
+            if (c > b)
+            {
+                if (c > a)
+                {
+                    Console.WriteLine("Наибольшее целое число - " + c);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
