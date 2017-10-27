032214003c2ee24c4189e1e7ce9eef955bf15507
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-27 17:07:26 +0300

    Сравнение чисел

---
+++ 06/Сравнение чисел/Сравнение чисел/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сравнение_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                comparison(">");
+            }
+            else if(a == b)
+            {
+                comparison("=");
+            }
+            else if(a < b)
+            {
+                comparison("<");
+            }
+
+        }
+        static void comparison(string Sign)
+        {
+            Console.WriteLine(Sign);
+            Console.ReadLine();
+        }
+        
+
+
+       
+        
+
+       
+    }
+}
