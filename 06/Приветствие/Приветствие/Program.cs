54ccbed6585edef7cb16592e70dc504049789005
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-27 16:36:18 +0300

    Привет,Ваня!

---
+++ 06/Приветствие/Приветствие/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("Ваня");
+        }
+        static void SayHello(string Name)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Привет," + Name + "!");
+            Console.ReadLine();
+        }
+
+    }
+}
