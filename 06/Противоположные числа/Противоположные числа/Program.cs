ff588636155e7191573c73bfc74f61bf0209ddbd
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-29 15:28:12 +0300

    Противоположные числа

---
+++ 06/Противоположные числа/Противоположные числа/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+            Opposite(a, b);
+         }
+        static void Opposite(int a, int b)
+        {
+            if (a > 0 && b > 0 || a == b )
+            {
+                Console.WriteLine(" + ");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(" - ");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
