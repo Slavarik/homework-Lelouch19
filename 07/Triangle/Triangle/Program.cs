6313a62ec5db2881ed954e6ba8e5baf20cfa6a85
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-05 14:51:48 +0300

    Существует ли такой треугольник?

---
+++ 07/Triangle/Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину первой стороны");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину второй стороны");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину третьей стороны");
+            int c = int.Parse(Console.ReadLine());
+            Triangle(a,b,c);
+        }
+        static void Triangle(int a, int b, int c)
+        {
+            if(a < (b + c) && b < (a + c) && c < (a + b))
+            {
+                Console.WriteLine("Такой треугольник может существовать");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Такой треугольник не может существовать");
+                Console.ReadLine(); 
+            }
+        
+
+        }
+
+
+    }
+}
