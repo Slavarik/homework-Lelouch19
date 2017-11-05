12d6667aa0496411acba8f06ab38968d378e7af2
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-05 15:08:42 +0300

    ShowMessage

---
+++ 07/ShowMessage/ShowMessage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMessage("Привет!", false);
+            ShowMessage("Внимание,внимание!", true);
+        }
+        static void ShowMessage(string message, bool isWarning)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
+            if (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+        }
+    }
+}
