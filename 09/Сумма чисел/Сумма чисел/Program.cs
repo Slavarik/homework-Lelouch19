5dfb0b5718c21f1b162afff28727822dd08905ed
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-19 16:49:47 +0300

    Залил файлы

---
+++ 09/Сумма чисел/Сумма чисел/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите число.");
+                int n = int.Parse(Console.ReadLine());
+                sum = sum + n;
+                Console.WriteLine("Сумма чисел равна - " + sum);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 09/Тренировщик арифметики/Тренировщик арифметики/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтобы закрыть меня,реши задачку.");
+            while (true)
+            {
+                Random rand = new Random();
+                int a = rand.Next(0, 10);
+                int b = rand.Next(0, 10);
+                int c = rand.Next(0, 10);
+                Console.WriteLine("Сколько будет " + a + " + " + b + " + " + c + " ?");
+                int answer = int.Parse(Console.ReadLine());
+                int sum = a + b + c;
+                Console.WriteLine();
+                if (answer == sum)
+                {
+                    Console.WriteLine("Ну ты и Мега-мозг!");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неа.Ну-ка, теперь попробуй решить другую");
+                }
+            }
+        }
+    }
+}
