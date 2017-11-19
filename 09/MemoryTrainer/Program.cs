ee71c994d612cde9dc062cecda86df4833f40cfb
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-19 15:55:16 +0300

    Тренировщик памяти

--- 09/MemoryTrainer/Program.cs
+++ 09/MemoryTrainer/Program.cs
@@ -5,31 +5,42 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MemoryTrainer {
-	class Program {
-		static void Main(string[] args) {
+namespace MemoryTrainer 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			GuessNumber();
 		}
 
-		static void GuessNumber() {
+		static void GuessNumber() 
+        {
 			Random rand = new Random();
+            int number = rand.Next(100);
+            int x = 100;
 
-			while (true) {
-				int number = rand.Next(100);
-
+			while (true) 
+            {
 				Console.WriteLine("Запомните число: " + number);
-				Thread.Sleep(2000);
+                Thread.Sleep(2000);
 				Console.Clear();
 
 				Console.WriteLine("Введите запомненное число");
 				int guess = int.Parse(Console.ReadLine());
 				Console.WriteLine();
 
-				if (guess == number) {
+				if (guess == number) 
+                {
 					Console.WriteLine("Вы угадали!");
+                    x = x + 200;
+                    number = rand.Next(x);
 				}
-				else {
+				else 
+                {
 					Console.WriteLine("Вы ошиблись ='(");
+                    x = x - 100;
+                    number = rand.Next(x);
 				}
 				Thread.Sleep(1000);
 				Console.Clear();
