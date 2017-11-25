0193445d2db6ab774a27a99e39856069e24b6a71
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-11-25 20:04:38 +0300

    Наброски кода

---
+++ 10/Квадраты натуральных чисел/Квадраты натуральных чисел/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Квадраты_натуральных_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число.");
+            int number = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            int c = rand.Next(0, number);
+            int b = number * number;
+            while (true)
+            {
+                if (c == b || c < b)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Квадраты не превосходящие " + number + " : ");
+                    Console.WriteLine(c);
+                    Console.ReadLine();
+                    break;
+                }
+               
+            }
+        }
+    }
+}
