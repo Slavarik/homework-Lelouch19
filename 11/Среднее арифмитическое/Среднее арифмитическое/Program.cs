04f52ab402a4fa9a5767de0e08c2e9a21c68510a
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-11-29 21:19:53 +0300

    Среднее арифмитическое 50 чисел
    На SoloLearn задания выполнил

---
+++ 11/Среднее арифмитическое/Среднее арифмитическое/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Среднее_арифмитическое
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int sum = 0;
+            while(n < 50)
+            {
+                Random rand = new Random();
+                int a = rand.Next(0, 500);
+                Console.WriteLine("Число - " + a);
+                sum = sum + a;
+                n = n + 1;               
+            }
+            Console.WriteLine("Среднее арифметическое - " + sum/50);
+            Console.ReadLine();  
+        }
+    }
+}
