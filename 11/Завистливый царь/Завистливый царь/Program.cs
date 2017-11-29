68a4416b66d56557616ace1e1bd5179d535a292d
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-11-29 21:29:37 +0300

    Завистливый царь и невезучие бояре

---
+++ 11/Завистливый царь/Завистливый царь/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Завистливый_царь
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Boyar = 0;
+            int TallBoyar = 0;
+            while (Boyar < 20)
+            {
+                Console.WriteLine("Введите рост боярина.");
+                int growth = int.Parse(Console.ReadLine());
+                if(growth <= 160)
+                {
+                    Boyar = Boyar + 1;
+                }
+                else
+                {
+                    Boyar = Boyar + 1;
+                    TallBoyar = TallBoyar + 1;
+                }
+            }
+            Console.WriteLine("Количество бояр,которых стоит наказать - " + TallBoyar);
+            Console.ReadLine();
+        }
+    }
+}
