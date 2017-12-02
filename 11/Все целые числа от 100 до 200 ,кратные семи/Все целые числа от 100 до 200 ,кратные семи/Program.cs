9c32f713d5197dc6c6154bfe8aadb36557dbc20f
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-02 21:44:58 +0300

    Все целые числа от 100 до 200,кратные 7

---
+++ 11/Все целые числа от 100 до 200 ,кратные семи/Все целые числа от 100 до 200 ,кратные семи/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Все_целые_числа_от_100_до_200__кратные_семи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int n = 0;
+            while (n < 14)
+            {  
+                int Number = rand.Next(100, 200);
+                int Rest = Number % 7;                                      //Остаток от деления на 7.
+                if(Rest == 0)
+                {
+                    Thread.Sleep(100);
+                    Console.WriteLine(Number);
+                    n = n + 1;
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 11/Все целые числа от 100 до 200 ,кратные семи/Все целые числа от 100 до 200 ,кратные семи/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Все целые числа от 100 до 200 ,кратные семи")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Все целые числа от 100 до 200 ,кратные семи")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1069c714-8f71-4fd7-be39-e1e2413707cc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
