7bba0116a9eb2ea90d5ade2b9567873ba5927b2a
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-02 22:15:41 +0300

    Буква "ж"

---
+++ 11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            //Код не работает.Просто наброски :(
+
+
+
+
+            Console.WriteLine("Введите любое слово");
+            string Word = Console.ReadLine();
+            Console.WriteLine(Word.Length);
+            int Long = Word.Length;         
+            int n = 0;    //Каждая буква
+            int je = 0;   //Буква ж
+            while (n < Long)
+            {
+                char letter = Word[n];
+                if (n == 'ж')
+                {
+                    je = je + 1;
+                }
+                n = n + 1;
+
+            }
+            Thread.Sleep(100);
+            Console.WriteLine("Количество букв 'ж' - " + je);
+            Console.ReadLine();
+
+        }
+    }
+}
